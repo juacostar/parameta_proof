7469523f64c494b298b1603f0c11f7aee186e9cc
Juan Camilo Acosta Rojas <juacostar@unal.edu.co>
2023-11-28 16:05:38 -0500

    deleting some files

--- Soap Client/spring-boot-soap-ws/src/main/java/com/javatechie/spring/soap/api/loaneligibility/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.11.27 a las 06:24:03 PM COT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.javatechie.com/spring/soap/api/loanEligibility", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.javatechie.spring.soap.api.loaneligibility;
